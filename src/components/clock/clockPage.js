4f9e9509844dcbd843be68c7c16eb9bee5d61600
Ryan Rasmussen <ryan.rasmussen@annelewisllc.com>
2019-07-01 18:48:12 -0400

    add auto-update on each second to clock component

--- src/components/clock/clockPage.js
+++ src/components/clock/clockPage.js
@@ -5,6 +5,24 @@ export default class ClockPage extends Component {
         super(props);
         this.state = {date: new Date()};
     }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+          () => this.tick(),
+          1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+          date: new Date()
+        });
+    }
+
     render() {
         return (
             <div>
